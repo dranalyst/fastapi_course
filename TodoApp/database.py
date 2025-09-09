d6093342446a84d7c0451886f136a0d4572e02d2
Douglas Raymond <dougraymond1@gmail.com>
2025-09-09 19:48:20 +0200

    production db change

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+#SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+SQLALCHEMY_DATABASE_URL = 'postgresql://todoappdb_xkjl_user:zFLvDaOwk1mQEO3gl4IsoxGUJHPV4pmq@dpg-d306eh75r7bs73b30tbg-a.frankfurt-postgres.render.com/todoappdb_xkjl'
 #SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:c6emcpostgres@localhost/todoApplicationDatabase'
 #SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root@127.0.0.1:3306/todoApplicationDatabase'
 
